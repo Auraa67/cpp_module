8953da41c335c9e66ec9da4b453e36ac394d909f
Razafindrasata Mendrika <razafindrasatamendrika@gmail.com>
2026-02-01 19:58:42 +0300

    first commit

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int	main(int argc, char const *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}	
+	for (int i = 1; argv[i]; i++)
+	{	
+		for (int j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] >= 97 && argv[i][j] <= 122)
+				std::cout << (char)(argv[i][j] - 32);
+			else
+				std::cout << (char)argv[i][j];
+		}
+		std::cout << ' ';
+	}
+	std::cout << std::endl;
+	return (0);
+}
