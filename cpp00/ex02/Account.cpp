7c2f9710940711728e00792f77458b663e389796
Mendrika <razafindrasatamendrika@gmail.com>
2026-02-06 12:38:18 +0300

    add: function head

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,29 @@
 /*   By: merazafi <merazafi@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 11:16:39 by merazafi          #+#    #+#             */
-/*   Updated: 2026/02/06 11:16:40 by merazafi         ###   ########.fr       */
+/*   Updated: 2026/02/06 11:55:38 by merazafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Account.hpp"
+
+void	makeDeposit( int deposit )
+{
+	
+}
+
+bool	makeWithdrawal( int withdrawal )
+{
+	
+}
+
+int		Account::checkAmount( void ) const
+{
+	return (0);
+}
+
+void	Account::displayStatus( void ) const
+{
+	
+}
 
